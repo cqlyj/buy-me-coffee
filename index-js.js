28c3cca871ca7e3d00e23f2e84367b972f68ca94
cqlyj <luoyingjie0721@gmail.com>
2025-05-02 11:48:08 +0800

    Add balance retrieval functionality and update UI for wallet interactions

--- index-js.js
+++ index-js.js
@@ -4,6 +4,7 @@ import {
   createPublicClient,
   parseEther,
   defineChain,
+  formatEther,
 } from "https://esm.sh/viem";
 
 import { contractAddress, abi } from "./constants-js.js";
@@ -11,6 +12,7 @@ import { contractAddress, abi } from "./constants-js.js";
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
 const ethAmountInput = document.getElementById("ethAmount");
+const balanceButton = document.getElementById("balanceButton");
 
 let walletClient;
 let publicClient;
@@ -77,5 +79,24 @@ async function getCurrentChain(client) {
   return currentChain;
 }
 
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      publicClient = createPublicClient({
+        transport: custom(window.ethereum),
+      });
+      const balance = await publicClient.getBalance({
+        address: contractAddress,
+      });
+      console.log(formatEther(balance));
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    balanceButton.innerHTML = "Please install MetaMask";
+  }
+}
+
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
